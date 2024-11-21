f34da835d01c40a22a8500f5249d798172f31a86
dev-ita <italo.ods@hotmail.com>
2024-11-21 13:30:40 -0300

    add CORS

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include "db/users.hpp"
 #include "routes/routes.hpp"
 #include <crow.h>
+#include <crow/app.h>
+#include <crow/middlewares/cors.h>
 
 int main() {
 
   UserRepository::setupDatabase();
 
-  crow::SimpleApp app;
+  crow::App<crow::CORSHandler> app;
+
+  auto &cors = app.get_middleware<crow::CORSHandler>();
+
+  cors.global()
+      .headers("X-Custom-Header", "Upgrade-Insecure-Requests", "Content-Type", "Origin", "Accept", "Authorization", "Refresh")
+      .methods("POST"_method, "GET"_method, "PUT"_method, "DELETE"_method)
+      .origin("*");
 
   CROW_ROUTE(app, "/register").methods("POST"_method)(registerUser);
   CROW_ROUTE(app, "/login").methods("POST"_method)(loginUser);
